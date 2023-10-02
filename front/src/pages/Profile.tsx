e7b723716e96d7630b08bf5b45520c6d032cfa75
Cyril BARTZ <bartzcyril@gmail.com>
2023-10-02 16:56:07 +0200

    ajout d'une vérification si l'ulisateur n'est pas connecté

--- front/src/pages/Profile.tsx
+++ front/src/pages/Profile.tsx
@@ -2,16 +2,32 @@ import {Header} from "../components/Header.tsx";
 import {Footer} from "../components/Footer.tsx";
 import {Account} from "../components/Account/Account.tsx";
 import {EditName} from "../components/Account/EditName.tsx";
+import {useSelector} from "react-redux";
+import {user, userSelector} from "../utils/type/user.ts";
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
 
 export function Profile() {
-    return (
-        <>
-            <Header/>
-            <main className="main bg-dark profile">
-                <EditName/>
-                <Account/>
-            </main>
-            <Footer/>
-        </>
-    )
+
+    const user: user = useSelector<userSelector>((u) => u.user) as user
+
+    if (user.token) {
+        return (
+            <>
+                <Header/>
+                <main className="main bg-dark profile">
+                    <EditName/>
+                    <Account/>
+                </main>
+                <Footer/>
+            </>
+        )
+    } else {
+        const navigate = useNavigate()
+        useEffect(() => {
+            navigate("/")
+        }, []);
+
+    }
+
 }
